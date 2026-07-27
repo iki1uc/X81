d3cc5c92e6f760681a6a1dafabe57519ad6185b1
iki1uc <entertrainer@web.de>
2026-07-27 16:42:56 +0200

    Update 3x81.kernel.js

--- JS/3x81.kernel.js
+++ JS/3x81.kernel.js
@@ -1,1 +1,15 @@
+export async function Kernel3x81() {
+    const Elektron = await fetch("../X81/X.tmp").then(r => r.text());
+    const Proton   = await fetch("../X81/q.tmp").then(r => r.text());
+    const Operator = await fetch("../X81/operator.tmp").then(r => r.text());
+    const Orbit    = await fetch("../X81/U.orbit").then(r => r.text());
+    const Neutron  = await fetch("../81.tmp/81.tmp").then(r => r.text());
 
+    return {
+        Elektron,
+        Proton,
+        Neutron,
+        Operator,
+        Orbit
+    };
+}
